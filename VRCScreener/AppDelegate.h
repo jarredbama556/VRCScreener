a9020d24da4a0c9d4612467258ea60bf03deb38c
jarredbama556 <jarredalldredge@Jarreds-MacBook-Pro.local>
2015-01-21 09:54:53 -0600

    Initial Commit

---
+++ VRCScreener/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  VRCScreener
+//
+//  Created by Jarred Alldredge on 1/21/15.
+//  Copyright (c) 2015 Vision Research. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ VRCScreener/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  VRCScreener
+//
+//  Created by Jarred Alldredge on 1/21/15.
+//  Copyright (c) 2015 Vision Research. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
